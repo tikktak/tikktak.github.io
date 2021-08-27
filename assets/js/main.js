78de89fbd785db86719c6ccb2ce1765103e1508e
tiktak <40335429+tikktak@users.noreply.github.com>
2021-08-27 13:44:11 +0200

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+   var copyText = "tiktak#0426";
+
+   navigator.clipboard.writeText(copyText);
+   
+   // console.log("Copied the text" + copyText);
+   alert("Succesfully copied the text!");
+}
